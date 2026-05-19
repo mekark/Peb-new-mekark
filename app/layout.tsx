ec98d531a8139be07cd63e5b8a9634f5a3c9e12c
NIRMAL RAJ M <126080219+nirmal2971@users.noreply.github.com>
2026-05-19 14:34:30 +0530

    Gogle tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Manrope, Inter, Space_Grotesk } from "next/font/google";
+import Script from "next/script";
 import FloatingWhatsApp from "@/components/Whatsapp";
 import "./globals.css";
+
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -19,19 +21,12 @@ const spaceGrotesk = Space_Grotesk({
   weight: ["400", "500", "700"],
 });
 
-
-
-
 export const metadata: Metadata = {
-  title:
-    "PEB Manufacturer in Chennai - Mekark",
+  title: "PEB Manufacturer in Chennai - Mekark",
 
   description:
     "Leading Pre-Engineered Building Manufacturer in Chennai. Turnkey PEB & EPC solutions – from design to execution with on-time delivery.",
 };
-
-
-
 
 export default function RootLayout({
   children,
@@ -40,6 +35,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5SBMM86H');
+            `,
+          }}
+        />
+      </head>
+
       <body
         className={`
           ${manrope.variable}
@@ -48,9 +64,20 @@ export default function RootLayout({
           antialiased
         `}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5SBMM86H"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         {children}
+
+        <FloatingWhatsApp />
       </body>
-      <FloatingWhatsApp />
     </html>
   );
 }
